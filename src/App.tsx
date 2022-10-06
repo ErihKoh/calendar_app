6b88bff8913e33be10c5506872f30eba5b3291b2
Yurii Zubar <63535943+ErihKoh@users.noreply.github.com>
2022-10-06 15:20:08 +0300

    add Calendar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import Calendar from "components/Calendar/";
 import React from "react";
 import "./App.css";
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <h2>My first calendar</h2>
+      <Calendar />
     </div>
   );
 };
