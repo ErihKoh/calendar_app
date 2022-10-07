385652f0c04e397f5d317244cd7f23eb48e04633
Yurii Zubar <63535943+ErihKoh@users.noreply.github.com>
2022-10-07 22:59:15 +0300

    completed calendar

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -1,19 +1,29 @@
 import { useCalendar } from 'components/hooks/useCalendar';
-import { checkDateIsEqual, checkIsToday, getWeekDaysNames } from 'utils/date';
+import { checkDateIsEqual, checkIsToday, getMonthNames, getWeekDaysNames } from 'utils/date';
 import style from './Calendar.module.css';
 interface ICalendarProps {
   locale?: string;
   selectedDate?: Date;
   selectDate: (date: Date) => void;
+  firstWeekDayNumber?: number;
 }
 
-const Calendar: React.FC<ICalendarProps> = ({ locale = 'default', selectDate, selectedDate }) => {
-  const { state, functions } = useCalendar({ locale, selectedDate });
+const Calendar: React.FC<ICalendarProps> = ({
+  locale = 'default',
+  selectDate,
+  selectedDate,
+  firstWeekDayNumber = 2
+}) => {
+  const { state, functions } = useCalendar({ locale, selectedDate, firstWeekDayNumber });
 
   return (
     <div className={style.calendar}>
       <div className={style.calendar__header}>
-        <div aria-hidden className={style.calendar__header__arrow__left}></div>
+        <div
+          aria-hidden
+          className={style.calendar__header__arrow__left}
+          onClick={() => functions.onClickArrow('left')}
+        ></div>
         {state.mode === 'days' && (
           <div aria-hidden onClick={() => functions.setMode('months')}>
             {state.monthsNames[state.selectedMonth.monthIndex].month} {state.selectedYear}
@@ -31,7 +41,11 @@ const Calendar: React.FC<ICalendarProps> = ({ locale = 'default', selectDate, se
             {state.selectedYearInterval[state.selectedYearInterval.length - 1]}
           </div>
         )}
-        <div aria-hidden className={style.calendar__header__arrow__right}></div>
+        <div
+          aria-hidden
+          className={style.calendar__header__arrow__right}
+          onClick={() => functions.onClickArrow('right')}
+        ></div>
       </div>
       <div className={style.calendar__body}>
         {state.mode === 'days' && (
@@ -49,7 +63,12 @@ const Calendar: React.FC<ICalendarProps> = ({ locale = 'default', selectDate, se
 
                 return (
                   <li
+                    aria-hidden
                     key={`${day.dayNumber}-${day.monthIndex}`}
+                    onClick={() => {
+                      functions.setSelectedDate(day);
+                      selectDate(day.date);
+                    }}
                     className={[
                       `${style.calendar__day}`,
                       isToday ? `${style.calendar__today__item}` : '',
@@ -63,6 +82,64 @@ const Calendar: React.FC<ICalendarProps> = ({ locale = 'default', selectDate, se
               })}
             </ul>
           </>
+        )}
+        {state.mode === 'months' && (
+          <ul className={style.calendar__pick__items__container}>
+            {state.monthsNames.map((monthsName) => {
+              const isCurrentMonth =
+                new Date().getMonth() === monthsName.monthIndex &&
+                new Date().getFullYear() === state.selectedYear;
+              const isSelectedMonth = monthsName.monthIndex === state.selectedMonth.monthIndex;
+              return (
+                <li
+                  aria-hidden
+                  onClick={() => {
+                    functions.setSelectedMonthByIndex(monthsName.monthIndex);
+                    functions.setMode('days');
+                  }}
+                  key={monthsName.month}
+                  className={[
+                    `${style.calendar__pick__item}`,
+                    isCurrentMonth ? `${style.calendar__today__item}` : '',
+                    isSelectedMonth ? `${style.calendar__selected__item}` : ''
+                  ].join(' ')}
+                >
+                  {monthsName.monthShort}
+                </li>
+              );
+            })}
+          </ul>
+        )}
+        {state.mode === 'years' && (
+          <div className={style.calendar__pick__items__container}>
+            <div className={style.calendar__unchoosable__year}>
+              {state.selectedYearInterval[0] - 1}
+            </div>
+            {state.selectedYearInterval.map((year) => {
+              const isCurrentYear = new Date().getFullYear() === year;
+              const isSelectedYear = year === state.selectedYear;
+              return (
+                <div
+                  aria-hidden
+                  onClick={() => {
+                    functions.setSelectedYear(year);
+                    functions.setMode('months');
+                  }}
+                  key={year}
+                  className={[
+                    `${style.calendar__pick__item}`,
+                    isCurrentYear ? `${style.calendar__today__item}` : '',
+                    isSelectedYear ? `${style.calendar__selected__item}` : ''
+                  ].join(' ')}
+                >
+                  {year}
+                </div>
+              );
+            })}
+            <div className={style.calendar__unchoosable__year}>
+              {state.selectedYearInterval[state.selectedYearInterval.length - 1] + 1}
+            </div>
+          </div>
         )}
       </div>
     </div>
--- src/components/hooks/useCalendar.ts
+++ src/components/hooks/useCalendar.ts
@@ -5,7 +5,10 @@ import { useMemo, useState } from 'react';
 interface IUseCalendarParams {
   locale?: string;
   selectedDate?: Date;
+  firstWeekDayNumber?: number;
 }
+
+const DAYS_IN_WEEK = 7;
 
 const getYearsInterval = (year: number) => {
   const startYear = Math.floor(year / 10) * 10;
@@ -16,7 +19,11 @@ const getYearsInterval = (year: number) => {
   ];
 };
 
-export const useCalendar = ({ locale = 'default', selectedDate: date }: IUseCalendarParams) => {
+export const useCalendar = ({
+  locale = 'default',
+  selectedDate: date,
+  firstWeekDayNumber = 2
+}: IUseCalendarParams) => {
   const [mode, setMode] = useState<'days' | 'months' | 'years'>('days');
   const [selectedDate, setSelectedDate] = useState(createDate({ date }));
   const [selectedMonth, setSelectedMonth] = useState(
@@ -33,7 +40,7 @@ export const useCalendar = ({ locale = 'default', selectedDate: date }: IUseCale
   const days = useMemo(() => selectedMonth.getDaysInMonth(), [selectedMonth]);
 
   const calendarAllDays = useMemo(() => {
-    const monthNumberOfDays = getCountDaysInMonth(selectedDate.monthIndex, selectedYear);
+    const monthNumberOfDays = getCountDaysInMonth(selectedMonth.monthIndex, selectedYear);
     const prevMonthDays = createMonth({
       date: new Date(selectedYear, selectedMonth.monthIndex - 1),
       locale
@@ -46,20 +53,77 @@ export const useCalendar = ({ locale = 'default', selectedDate: date }: IUseCale
     const firstDay = days[0];
     const lastDay = days[monthNumberOfDays - 1];
 
+    const shiftIndex = firstWeekDayNumber - 1;
+
     const numberOfPrevDays =
-      firstDay.dayNumberInWeek - 1 < 0
-        ? 7 - firstDay.dayNumberInWeek
-        : firstDay.dayNumberInWeek - 1;
+      firstDay.dayNumberInWeek - 1 - shiftIndex < 0
+        ? DAYS_IN_WEEK - (firstWeekDayNumber - firstDay.dayNumberInWeek)
+        : firstDay.dayNumberInWeek - 1 - shiftIndex;
 
-    const numberOfNextDays = lastDay.dayNumberInWeek - 1 < 6 ? 7 - lastDay.dayNumberInWeek : 0;
+    const numberOfNextDays =
+      DAYS_IN_WEEK - lastDay.dayNumberInWeek + shiftIndex > 6
+        ? DAYS_IN_WEEK - lastDay.dayNumberInWeek - (DAYS_IN_WEEK - shiftIndex)
+        : DAYS_IN_WEEK - lastDay.dayNumberInWeek + shiftIndex;
 
-    const visiblePrevDays = prevMonthDays.slice(-(numberOfPrevDays - 1));
+    const totalCalendarDays = days.length + numberOfPrevDays + numberOfNextDays;
 
-    const visibleNextDay = nextMonthDays.slice(0, numberOfNextDays + 1);
+    const result = [];
 
-    return [...visiblePrevDays, ...days, ...visibleNextDay];
-  }, [selectedDate.monthIndex, selectedYear]);
+    for (let i = 0; i < numberOfPrevDays; i += 1) {
+      const inverted = numberOfPrevDays - i;
+      result[i] = prevMonthDays[prevMonthDays.length - inverted];
+    }
 
+    for (let i = numberOfPrevDays; i < totalCalendarDays - numberOfNextDays; i += 1) {
+      result[i] = days[i - numberOfPrevDays];
+    }
+
+    for (let i = totalCalendarDays - numberOfNextDays; i < totalCalendarDays; i += 1) {
+      result[i] = nextMonthDays[i - totalCalendarDays + numberOfNextDays];
+    }
+
+    return result;
+  }, [selectedMonth.year, selectedMonth.monthIndex, selectedYear]);
+
+  const onClickArrow = (direction: 'right' | 'left') => {
+    if (mode === 'years' && direction === 'left') {
+      return setSelectedYearInterval(getYearsInterval(selectedYearInterval[0] - 10));
+    }
+    if (mode === 'years' && direction === 'right') {
+      return setSelectedYearInterval(getYearsInterval(selectedYearInterval[0] + 10));
+    }
+    if (mode === 'months' && direction === 'left') {
+      const year = selectedYear - 1;
+      if (!selectedYearInterval.includes(year)) {
+        setSelectedYearInterval(getYearsInterval(year));
+      }
+      return setSelectedYear(year);
+    }
+    if (mode === 'months' && direction === 'right') {
+      const year = selectedYear + 1;
+      if (!selectedYearInterval.includes(year)) {
+        setSelectedYearInterval(getYearsInterval(year));
+      }
+      return setSelectedYear(year);
+    }
+    if (mode === 'days') {
+      const monthIndex =
+        direction === 'left' ? selectedMonth.monthIndex - 1 : selectedMonth.monthIndex + 1;
+      if (monthIndex === -1) {
+        const year = selectedYear - 1;
+        setSelectedYear(year);
+        if (!selectedYearInterval.includes(year)) {
+          setSelectedYearInterval(getYearsInterval(year));
+        }
+        return setSelectedMonth(createMonth({ date: new Date(selectedYear - 1, 11), locale }));
+      }
+      setSelectedMonth(createMonth({ date: new Date(selectedYear, monthIndex), locale }));
+    }
+  };
+
+  const setSelectedMonthByIndex = (monthIndex: number) => {
+    setSelectedMonth(createMonth({ date: new Date(selectedYear, monthIndex), locale }));
+  };
   return {
     state: {
       mode,
@@ -72,7 +136,11 @@ export const useCalendar = ({ locale = 'default', selectedDate: date }: IUseCale
       selectedYearInterval
     },
     functions: {
-      setMode
+      setMode,
+      setSelectedDate,
+      onClickArrow,
+      setSelectedMonthByIndex,
+      setSelectedYear
     }
   };
 };
